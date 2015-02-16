5b8bfe72febd665b23ab8f8eb3a0144cbb2dfa53
felipedau <dau.felipe@gmail.com>
2015-02-16 18:07:23 -0200

    Validate nym email address

--- nymphemeral/nym.py
+++ nymphemeral/nym.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python
+import re
 
+from errors import *
 
 class Nym():
     def __init__(self, address, passphrase=None, fingerprint=None, hsub=None):
+        if not re.match(r'[^@]+@[^@]+\.[^@]+', address):
+            raise InvalidEmailAddressError(address)
         self.address = address
         self.passphrase = passphrase
         self.fingerprint = fingerprint
