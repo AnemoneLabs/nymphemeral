698d5275773b6313fd93638b90c2467313168e80
Felipe Dau <dau.felipe@gmail.com>
2015-07-05 19:42:40 -0300

    Move docstring and attributes to __init__.py

---
+++ nymphemeral/__init__.py
@@ -0,0 +1,36 @@
+"""
+nymphemeral - an ephemeral nymserver GUI client
+
+Messages are retrieved from a.a.m using aampy.py and hsub.py
+from https://github.com/rxcomm/aampy
+
+Messages dates are parsed using python-dateutil 2.2 from
+https://pypi.python.org/pypi/python-dateutil
+
+Encryption is done using python-gnupg and pyaxo from
+https://pypi.python.org/pypi/python-gnupg/
+https://github.com/rxcomm/pyaxo
+
+Copyright (C) 2014 by Felipe Dau <dau.felipe@gmail.com> and
+David R. Andersen <k0rx@RXcomm.net>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+For more information, see https://github.com/felipedau/nymphemeral
+"""
+
+__author__ = 'Felipe Dau and David R. Andersen'
+__license__ = 'GPL'
+__version__ = '1.3.2.5'
+__status__ = 'Beta'
