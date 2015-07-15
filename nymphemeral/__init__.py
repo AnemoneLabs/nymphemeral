54019190cc67b4d3e2b4f3e656c17c52df498794
Felipe Dau <dau.felipe@gmail.com>
2015-07-15 01:46:29 -0300

    Bump version

--- nymphemeral/__init__.py
+++ nymphemeral/__init__.py
@@ -32,7 +32,7 @@ For more information, see https://github.com/felipedau/nymphemeral
 
 __author__ = 'Felipe Dau and David R. Andersen'
 __license__ = 'GPL'
-__version__ = '1.3.2.6'
+__version__ = '1.3.2.7'
 __status__ = 'Beta'
 
 import logging
