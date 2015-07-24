ed9e7151f425e62f7c530d85cd38b455c36855c5
Felipe Dau <dau.felipe@gmail.com>
2015-07-24 00:42:17 -0300

    Use NymphemeralError's initializer in its subclasses

--- nymphemeral/errors.py
+++ nymphemeral/errors.py
@@ -6,119 +6,154 @@ class NymphemeralError(Exception):
 
 class AmbiguousUidError(NymphemeralError):
     def __init__(self, uid):
-        self.title = 'Ambiguous UID'
-        self.message = '"' + uid + \
-                       '" has multiple fingerprints. Be more specific.'
+        super(AmbiguousUidError, self).__init__(
+            title='Ambiguous UID',
+            message='"' + uid +
+                    '" has multiple fingerprints. Be more specific.'
+        )
 
 
 class InvalidEmailAddressError(NymphemeralError):
     def __init__(self, address):
-        self.title = 'Invalid Email Address'
-        self.message = '"' + address + '" is not in a valid format.'
+        super(InvalidEmailAddressError, self).__init__(
+            title='Invalid Email Address',
+            message='"' + address + '" is not in a valid format.'
+        )
 
 
 class InvalidPassphraseError(NymphemeralError):
     def __init__(self):
-        self.title = 'Invalid Passphrase'
-        self.message = 'The passphrase provided (empty) is not valid.'
+        super(InvalidPassphraseError, self).__init__(
+            title='Invalid Passphrase',
+            message='The passphrase provided (empty) is not valid.'
+        )
 
 
 class NymservNotFoundError(NymphemeralError):
     def __init__(self, nymserv):
-        self.title = 'Nym Server Not Found'
-        self.message = '"' + nymserv + \
-                       '" public key could not be found in the keyring.'
+        super(NymservNotFoundError, self).__init__(
+            title='Nym Server Not Found',
+            message='"' + nymserv +
+                    '" public key could not be found in the keyring.'
+        )
 
 
 class NymNotFoundError(NymphemeralError):
     def __init__(self, nym):
-        self.title = 'Nym Not Found'
-        self.message = '"' + nym + '" does not exist.'
+        super(NymNotFoundError, self).__init__(
+            title='Nym Not Found',
+            message='"' + nym + '" does not exist.'
+        )
 
 
 class NewsserverNotFoundError(NymphemeralError):
     def __init__(self, server, port):
-        self.title = 'Socket Error'
-        self.message = 'The server running on ' + \
-                       server + ':' + str(port) + ' could not be found.'
+        super(NewsserverNotFoundError, self).__init__(
+            title='Socket Error',
+            message='The server running on ' +
+                    server + ':' + str(port) + ' could not be found.'
+        )
 
 
 class FingerprintNotFoundError(NymphemeralError):
     def __init__(self, query):
-        self.title = 'Fingerprint Not Found'
-        self.message = 'The fingerprint for "' + query + \
-                       '" could not be found in the keyring.'
+        super(FingerprintNotFoundError, self).__init__(
+            title='Fingerprint Not Found',
+            message='The fingerprint for "' + query +
+                    '" could not be found in the keyring.'
+        )
 
 
 class KeyNotFoundError(NymphemeralError):
     def __init__(self, query):
-        self.title = 'Key Not Found'
-        self.message = 'The key for "' + query + \
-                       '" could not be found in the keyring.'
+        super(KeyNotFoundError, self).__init__(
+            title='Key Not Found',
+            message='The key for "' + query +
+                    '" could not be found in the keyring.'
+        )
 
 
 class SecretKeyNotFoundError(NymphemeralError):
     def __init__(self, query):
-        self.title = 'Scret Key Not Found'
-        self.message = 'The secret key for "' + query + \
-                       '" could not be found in the keyring.'
+        super(SecretKeyNotFoundError, self).__init__(
+            title='Scret Key Not Found',
+            message='The secret key for "' + query +
+                    '" could not be found in the keyring.'
+        )
 
 
 class IncorrectPassphraseError(NymphemeralError):
     def __init__(self):
-        self.title = 'Incorrect Passphrase'
-        self.message = 'The passphrase provided is incorrect.'
+        super(IncorrectPassphraseError, self).__init__(
+            title='Incorrect Passphrase',
+            message='The passphrase provided is incorrect.'
+        )
 
 
 class InvalidHsubError(NymphemeralError):
     def __init__(self):
-        self.title = 'Invalid hSub Passphrase'
-        self.message = 'The hSub passphrase provided (empty) is not valid.'
+        super(InvalidHsubError, self).__init__(
+            title='Invalid hSub Passphrase',
+            message='The hSub passphrase provided (empty) is not valid.'
+        )
 
 
 class InvalidEphemeralKeyError(NymphemeralError):
     def __init__(self):
-        self.title = 'Invalid Ephemeral Key'
-        self.message = 'The ephemeral key provided (empty) is not valid.'
+        super(InvalidEphemeralKeyError, self).__init__(
+            title='Invalid Ephemeral Key',
+            message='The ephemeral key provided (empty) is not valid.'
+        )
 
 
 class InvalidNameError(NymphemeralError):
     def __init__(self):
-        self.title = 'Invalid Name'
-        self.message = 'The pseudonymous provided (empty) is not valid.'
+        super(InvalidNameError, self).__init__(
+            title='Invalid Name',
+            message='The pseudonymous provided (empty) is not valid.'
+        )
 
 
 class InvalidDurationError(NymphemeralError):
     def __init__(self):
-        self.title = 'Invalid Duration'
-        self.message = ('The duration provided should be in the format:\n\n'
-                        '0 = key does not expire\n'
-                        '<n> = key expires in n days\n'
-                        '<n>w = key expires in n weeks\n'
-                        '<n>m = key expires in n months\n'
-                        '<n>y = key expires in n years')
+        super(InvalidDurationError, self).__init__(
+            title='Invalid Duration',
+            message='The duration provided should be in the format:\n\n'
+                    '0 = key does not expire\n'
+                    '<n> = key expires in n days\n'
+                    '<n>w = key expires in n weeks\n'
+                    '<n>m = key expires in n months\n'
+                    '<n>y = key expires in n years'
+        )
 
 
 class UndecipherableMessageError(NymphemeralError):
     def __init__(self):
-        self.title = 'Undecipherable Message'
-        self.message = 'The message could not be deciphered.'
+        super(UndecipherableMessageError, self).__init__(
+            title='Undecipherable Message',
+            message='The message could not be deciphered.'
+        )
 
 
 class EmptyTargetError(NymphemeralError):
     def __init__(self):
-        self.title = 'Empty Target'
-        self.message = 'The message must have a target.'
+        super(EmptyTargetError, self).__init__(
+            title='Empty Target',
+            message='The message must have a target.'
+        )
 
 
 class EmptyBodyError(NymphemeralError):
     def __init__(self):
-        self.title = 'Empty Body'
-        self.message = 'The message must have a body.'
+        super(EmptyBodyError, self).__init__(
+            title='Empty Body',
+            message='The message must have a body.'
+        )
 
 
 class EmptyChangesError(NymphemeralError):
     def __init__(self):
-        self.title = 'Empty Changes'
-        self.message = 'The configuration message must have at least one ' + \
-                       'change.'
+        super(EmptyChangesError, self).__init__(
+            title='Empty Changes',
+            message='The configuration message must have at least one change.'
+        )
